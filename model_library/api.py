e386cd32184eae3e5fbe05ffdbd6164096bb8f56
Adam Tomkins <a.tomkins@digital-science.com>
2024-03-04 11:55:54 +0000

    fix auth

--- model_library/api.py
+++ model_library/api.py
@@ -1,4 +1,8 @@
-from fastapi import FastAPI, HTTPException
+import os
+
+from fastapi import FastAPI, HTTPException, Depends
+from starlette.requests import Request
+
 
 from model_library.models import (
     ModelDeploymentTemplateCard,
@@ -11,19 +15,42 @@ from model_library.utils import create_deployment_yaml
 
 library = ModelLibrary()
 
+
+
+# Configuration handling
+# Conservatively assume Auth is enabled if anything other than these false values are set.
+USE_AUTH = not os.getenv("ML_USE_AUTH", "True").lower() in ("false", "0", "f", "no")
+MASTER_KEY = os.getenv("ML_MASTER_KEY")
+
+if USE_AUTH:
+    assert MASTER_KEY is not None, "If you are using auth, you must set a master key using the 'ML_MASTER_KEY' environment variable."
+else:
+    logger.warning("Warning: Authentication is disabled. This should only be used for testing.")
+
+
 # Create a FastAPI app
 app = FastAPI()
 
+# API Key Validation
+async def verify_api_key(request: Request):
+    if not USE_AUTH:
+        return
+    api_key = request.headers.get("X-API-KEY")
+    if api_key != MASTER_KEY:
+        print(api_key, MASTER_KEY)
+        raise HTTPException(status_code=401, detail="Invalid API Key")
+    return api_key
 
 # Get all endpoint
 @app.get("/get_all")
-def get_all() -> Dict[str, Any]:
+def get_all(api_key: str = Depends(verify_api_key)) -> Dict[str, Any]:
     return library.to_dict()
 
 
 @app.post("/create")
 def create_kubernetes_deployment(
-    template: ModelDeploymentTemplateCard, user: UserInformation
+    template: ModelDeploymentTemplateCard, user: UserInformation, 
+    api_key: str = Depends(verify_api_key)
 ):
     card = ModelDeploymentCard(
         model_deployment_template=template, user_information=user
@@ -36,7 +63,10 @@ def create_kubernetes_deployment(
 
 
 @app.post("/validate_template")
-def validate_template(template: ModelDeploymentTemplateCard):
+def validate_template(
+    template: ModelDeploymentTemplateCard,
+    api_key: str = Depends(verify_api_key)
+    ):
     return True
 
 
--- model_library/client.py
+++ model_library/client.py
@@ -6,27 +6,31 @@ class ModelLibraryClient:
     def __init__(self, base_url: str, api_key: str = None):
         self.base_url = base_url
         self.api_key = api_key
+        self.headers = {}
+        if api_key:
+            self.headers['X-API-KEY'] = self.api_key
+
 
     def get_all(self) -> Dict[str, Any]:
-        response = requests.get(f"{self.base_url}/get_all")
+        response = requests.get(f"{self.base_url}/get_all", headers=self.headers)
         return response.json()
 
     def create_kubernetes_deployment(self, template: Dict, user: Dict) -> Any:
         data = {"template": template, "user": user}
 
-        response = requests.post(f"{self.base_url}/create", json=data)
+        response = requests.post(f"{self.base_url}/create", json=data, headers=self.headers)
         print(response)
         return response.json()
 
     def validate_template(self, template: Dict) -> bool:
         response = requests.post(
-            f"{self.base_url}/validate_template", json={"template": template}
+            f"{self.base_url}/validate_template", json={"template": template}, headers=self.headers
         )
         return response.json()
 
     def health(self) -> bool:
         try:
-            response = requests.get(f"{self.base_url}/health")
+            response = requests.get(f"{self.base_url}/health", headers=self.headers)
         except:
             return False
         return response.status_code == 200
